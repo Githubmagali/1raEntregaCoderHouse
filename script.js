efdc61d7bc4ec34e6042f778e6e3b0365219c9fa
Githubmagali <106199984+Githubmagali@users.noreply.github.com>
2023-08-03 18:06:49 -0300

    cambios

--- script.js
+++ script.js
@@ -26,3 +26,48 @@ function zoomOut(elemento){
     elemento.style.transform ="scale(1)";
 }
 
+
+
+async function mostrarDatos(){
+    try{
+        const response = await fetch ("http://127.0.0.1:5500/data.json");
+        const jsonData = await response.json();
+        const contenedor = document.getElementById('sponsor_1');
+
+        jsonData.forEach((item)=>{
+            const nombre =item.name;
+            const p = document.createElement("p");
+            p.textContent= nombre;
+            contenedor.appendChild(p);
+        });
+    }catch (error){
+        console.error('Error al mostrar los datos:', error);
+    }
+}
+mostrarDatos();
+/*
+async function sponsor(){
+    const response = await fetch("");
+    const json = await response.json();
+   
+    const contenedor = document.getElementById('');
+    contenedor.textContent= json.name;
+}
+sponsor();
+
+
+let sponsor =document.querySelector("#sponsor_1");
+fetch("data.json")
+.then((resp) => resp.json())
+.then((data)=>{
+    console.log(data.results);
+
+    data.map((item)=>{
+        const content = document.createElement("div");
+        content.innerHTML = `
+        <h4>${item.name}</h4>
+        `;
+        sponsor.append(content);
+    });
+});
+*/
