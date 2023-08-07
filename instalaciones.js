8571fa1f7927ad7c7cd49cc5b5e50966b37aa14d
Githubmagali <106199984+Githubmagali@users.noreply.github.com>
2023-08-07 19:44:52 -0300

    cambios

--- instalaciones.js
+++ instalaciones.js
@@ -16,3 +16,37 @@ if(entriesn[0].isIntersecting){
 });
 
 observer00.observe(galeria00);
+
+const fotos = document.getElementById('primera-clase');
+const fotos2 = document.getElementById('ventana-trabajos');
+
+const datos=[{
+    id:"1",
+    titulo:"Cabinas privadas",
+    texto: "Disfruta de tu privacidad al 100%"},{
+    id:"2",
+    titulo:"Asientos eco cuero",
+    texto: "Asientos reclinabes"},{
+     id:"3",
+     titulo:"Atencion personalizada",
+     texto: "Profe apruebeme con diez"}
+];
+
+fotos.addEventListener('click', (e) =>{
+    e.preventDefault();
+    const fotoClick =e.target.closest('.box-img');
+    if (fotoClick){
+        const id= fotoClick.dataset.id;
+
+         const datosc = datos.filter((datosc)=>{
+          if(datosc.id === id){
+             return datosc;
+          }
+
+        });
+        const {titulo, texto} = datosc[0];
+        fotos2.querySelector('.ventana__titulo').innerText = titulo;
+        fotos2.querySelector('.ventana__parrafo').innerText = texto;
+        fotos2.classList.add('ventana-active');
+    }
+});
